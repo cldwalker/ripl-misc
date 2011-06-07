350e687571fec584aab326277f7bff67a799087d
Gabriel Horner <gabriel.horner@gmail.com>
2011-06-07 00:37:28 -0400

    first stab at EM in ripl shell

---
+++ lib/ripl/em.rb
@@ -0,0 +1,51 @@
+require 'ripl'
+require 'eventmachine'
+
+# Runs eventmachine code in a ripl shell - asynchronously of course
+# TODO: Readline history, autocompletion and Ctrl-D exit
+module Ripl::Em
+  def self.included(mod)
+    meth = mod.instance_method(:loop)
+    mod.send(:define_method, :loop) do
+      catch(:ripl_exit) {
+        EM.run { meth.bind(Ripl.shell).call }
+      }
+    end
+  end
+
+  def get_input() @input end
+
+  def before_loop
+    $stdout.sync = true
+    super
+    trap("SIGINT") { handle_interrupt }
+    EM.open_keyboard(KeyboardHandler)
+  end
+end
+
+class KeyboardHandler < EM::Connection
+  include EM::Protocols::LineText2
+
+  def post_init
+    print Ripl.shell.prompt
+  end
+
+  def xreceive_data(data)
+    if data[/\t$/]
+      warn "TAB #{data}"
+      Bond.agent.call(data.chop, data.chop)
+    else
+      super
+    end
+  end
+
+  def receive_line(line)
+    # not in latest ripl yet
+    Ripl.shell.input = line
+    Ripl.shell.loop_once
+    print Ripl.shell.prompt
+  end
+end
+
+Ripl.config[:readline] = false
+Ripl::Shell.include Ripl::Em
